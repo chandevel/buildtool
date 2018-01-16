b28538f7bd6909445b2e331656802aea8b3eca99
Floens <floens@gmail.com>
2018-01-16 19:50:41 +0100

    initial commit

---
+++ app.py
@@ -0,0 +1,239 @@
+import datetime
+import json
+import os.path
+import shutil
+
+import docker
+import yaml
+from docker.errors import ContainerError
+from git import Repo
+
+
+class Configuration:
+    def __init__(self, name, git_url, branch, repo_path):
+        self.name = name
+        self.git_url = git_url
+        self.branch = branch
+        self.repo_path = repo_path
+
+        self.mode = ''
+
+        self.command = None
+        self.command_path = None
+
+        self.keys = []
+        self.artifacts = []
+        self.output_path = None
+        self.version = None
+        self.version_out = None
+        self.version_url = None
+
+        self.sha = None
+
+        self.docker_file = None
+        self.docker_tag = None
+        self.docker_repo_path = None
+
+
+class Key:
+    def __init__(self, from_file, to_file):
+        self.from_file = from_file
+        self.to_file = to_file
+
+
+class Artifact:
+    def __init__(self, from_file, to_file):
+        self.from_file = from_file
+        self.to_file = to_file
+
+
+def load_configuration(filename, branch):
+    with open(filename, 'r') as f:
+        r = yaml.load(f)
+
+        # Top level
+        name = r['name']
+        repo = r['repo']
+
+        if branch not in r['branches']:
+            return None
+
+        repo_path = os.path.join('data', 'git', name)
+        c = Configuration(name, repo, branch, repo_path)
+
+        # Output
+        output_config = r['output']
+        c.output_path = output_config['path']
+
+        # Docker
+        docker_config = r['docker']
+        c.docker_file = docker_config['file']
+        c.docker_tag = docker_config['tag']
+        c.docker_repo_path = docker_config['repo_path']
+
+        # Branches
+        branch_config = r['branches'][branch]
+
+        c.mode = branch_config['mode']
+        if c.mode not in ('tags', 'hash'):
+            raise ValueError('mode must be "tags" or "hash"')
+
+        c.command = branch_config['command']
+        c.command_path = branch_config['path']
+
+        keys = branch_config['keys']
+        for key in keys:
+            c.keys.append(Key(key['from'], key['to']))
+
+        artifacts = branch_config['artifacts']
+        for artifact in artifacts:
+            c.artifacts.append(Artifact(artifact['from'], artifact['to']))
+
+        c.version_out = branch_config['version_out']
+        c.version_url = branch_config['version_url']
+
+        return c
+
+
+def process_repo(repo, conf):
+    conf.sha = repo.head.commit.hexsha
+
+    for key in conf.keys:
+        from_file = key.from_file
+        to_file = key.to_file
+
+        shutil.copy(
+            os.path.join(from_file),
+            os.path.join(conf.repo_path, to_file)
+        )
+
+
+def gather_repo(conf):
+    print('gathering code')
+
+    path = conf.repo_path
+    try:
+        # Create if nonexistent
+        os.makedirs(path)
+        repo = Repo.clone_from(conf.git_url, path)
+    except FileExistsError:
+        # Reuse if exists
+        repo = Repo(path)
+    # Reset to branch
+    branch = repo.create_head(conf.branch)
+    repo.head.reference = branch
+    repo.head.reset(index=True, working_tree=True)
+
+    return repo
+
+
+def execute_build(repo, conf, docker_client):
+    print('running build for {}/{}'.format(conf.branch, conf.sha))
+
+    with open(conf.docker_file, 'rb') as docker_file:
+        print('docker image setup')
+        image = docker_client.images.build(
+            fileobj=docker_file, tag=conf.docker_tag, rm=True)
+
+        repo_absolute_path = os.path.abspath(conf.repo_path)
+
+        try:
+            container = docker_client.containers.run(
+                image,
+                detach=True,
+                volumes={
+                    repo_absolute_path: {
+                        'bind': conf.docker_repo_path,
+                        'mode': 'rw'
+                    }
+                },
+                command=conf.command,
+                working_dir=os.path.join(
+                    conf.docker_repo_path, conf.command_path)
+            )
+            a = container.attach(stdout=True, stderr=True, stream=True)
+            for l in a:
+                print(l.decode('utf8'), end='')
+
+        except ContainerError as e:
+            raise Exception('Build failed', e)
+
+
+def process_artifacts(repo, conf):
+    os.makedirs(conf.output_path, exist_ok=True)
+
+    if conf.mode == 'hash':
+        conf.version = conf.sha[:8]
+    elif conf.mode == 'tags':
+        conf.version = 'TODO_TAG_HERE'
+
+    for artifact in conf.artifacts:
+        version = conf.version
+
+        from_file = artifact.from_file
+        to_file = artifact.to_file.replace('%v', version)
+
+        shutil.copy(
+            os.path.join(conf.repo_path, from_file),
+            os.path.join(conf.output_path, to_file)
+        )
+
+
+def write_version(repo, conf):
+    with open(conf.version_out, 'w') as f:
+        url = conf.version_url.replace('%v', conf.version)
+
+        message = {
+            'type': 'update',
+            'date': datetime.datetime.today().strftime('%Y-%m-%dT%H:%M:%S'),
+            'message_html': '<h2>Clover update ready</h2>A new Clover dev build is ready.',
+
+            'apk': {
+                'dev': {
+                    'url': url
+                }
+            }
+        }
+
+        if conf.mode == 'hash':
+            message['hash'] = conf.sha[:8]
+        elif conf.mode == 'tags':
+            message['code'] = 1337  # TODO: get code
+
+        version = {
+            'api_version': 1,
+            'messages': [message],
+            'check_interval': 432000000
+        }
+
+        json.dump(version, f, indent=4)
+
+
+def run_with_configuration(conf: Configuration):
+    docker_client = get_docker()
+
+    repo = gather_repo(conf)
+    process_repo(repo, conf)
+    execute_build(repo, conf, docker_client)
+    process_artifacts(repo, conf)
+    write_version(repo, conf)
+
+
+def get_docker():
+    docker_client = docker.from_env()
+    try:
+        docker_client.info()
+    except Exception as e:
+        raise Exception('Docker not found: %s' % e)
+    return docker_client
+
+
+def run(branch):
+    conf = load_configuration('config.yml', branch)
+    # If a configuration exists for the branch.
+    if conf:
+        run_with_configuration(conf)
+
+
+if __name__ == '__main__':
+    run('dev')
---
+++ server.py
@@ -0,0 +1,82 @@
+import hmac
+from concurrent.futures import ThreadPoolExecutor
+from ipaddress import ip_address, ip_network
+
+import requests
+import yaml
+from flask import Flask, request, abort, json
+
+from app import run as run_build
+
+app = Flask(__name__)
+
+pool = ThreadPoolExecutor(1)
+
+hook_secret = None
+
+
+def run_build_on_pool(branch):
+    pool.submit(run_build, branch)
+
+
+def check_origin():
+    src_ip = ip_address(u'{}'.format(request.remote_addr))
+    whitelist = requests.get('https://api.github.com/meta').json()['hooks']
+
+    whitelist += ['127.0.0.1']
+
+    for valid_ip in whitelist:
+        if src_ip in ip_network(valid_ip):
+            break
+    else:
+        abort(403)
+
+
+def check_secret(provided_secret):
+    if not provided_secret:
+        abort(403)
+
+    sha_name, signature = provided_secret.split('=')
+    if sha_name != 'sha1':
+        abort(501)
+
+    mac = hmac.new(hook_secret.encode(), msg=request.data, digestmod='sha1')
+
+    if not hmac.compare_digest(str(mac.hexdigest()), str(signature)):
+        abort(403)
+
+
+@app.route('/hook', methods=['POST'])
+def hook():
+    res = request.get_json()
+    if not res:
+        abort(400)
+
+    check_origin()
+    check_secret(request.headers['X-Hub-Signature'])
+
+    event = request.headers['X-GitHub-Event']
+    if event in ('ping', 'push'):
+        if event == 'ping':
+            return json.dumps({
+                'msg': 'pong'
+            })
+        elif event == 'push':
+            branch = res['branch'].rsplit('/', maxsplit=1)[1]
+            run_build_on_pool(branch)
+
+    return 'ok'
+
+
+def run_server():
+    global hook_secret
+
+    with open('config.yml', 'r') as f:
+        r = yaml.load(f)
+        hook_secret = r['hook_secret']
+
+    app.run()
+
+
+if __name__ == '__main__':
+    run_server()
